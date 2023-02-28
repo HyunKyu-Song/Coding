5b0fd90cb366deb4dd70cde378187aed8e623b79
송현규 <108565747+HyunKyu-Song@users.noreply.github.com>
2023-02-28 23:54:32 +0900

    백준

---
+++ CodingTest/백준/별 찍기6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+	
+	int n, i, j;
+	
+	scanf("%d", &n);
+	
+	for(i = n; i > 0; i--){
+		for(j = i; j < n; j++){
+			printf(" ");
+		}
+		for(j = 2*i-1; j >= 1 ; j--){
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
