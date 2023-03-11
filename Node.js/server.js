2a45437b7ac9ca48af06b1d63cb63c0e631c3017
Song <108565747+HyunKyu-Song@users.noreply.github.com>
2023-03-11 17:25:35 +0900

    Add files via upload

--- Node.js/server.js
+++ Node.js/server.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({extended: true})) 
+app.use(express.urlencoded({ extended: true }));
+const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
 
-app.listen(8080, function(){
-   console.log('listening on 8080');
+var db;
+MongoClient.connect('mongodb+srv://song0726:song033634120@cluster0.jkh1uqu.mongodb.net/?retryWrites=true&w=majority', {useUnifiedTopology: true}, function (err, client) {
+   if (err) return console.log(err);
+   db = client.db('todolist');
+
+   app.listen(8080, function () {
+      console.log('listening 8080');
+   });
 })
 
-app.get('/', function(req, res){
-   res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+   db.collection('post').findOne({work: '공부'}, function(err, result){
+      res.render('list.ejs', {posts : result});
+      console.log(result);
+   })
 })
 
-app.get('/write', function(req, res){
-   res.sendFile(__dirname + '/write.html');
+app.get('/write', function (req, res) {
+   res.render('write.ejs');
 })
 
-app.post('/add', function(req, res){
-   console.log(req.body.todayWork);
+app.post('/add', function (req, res) {
+   db.collection('post').insertOne({_id : 2, work : req.body.todayWork, content : req.body.detailContent});
    res.send('전송완료');
 })
