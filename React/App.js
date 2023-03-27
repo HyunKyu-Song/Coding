1f4d9fe049ce98afc05a5dc1c7ec484e6b0c11c5
Song <108565747+HyunKyu-Song@users.noreply.github.com>
2023-03-27 23:00:08 +0900

    React

--- React/App.js
+++ React/App.js
@@ -6,16 +6,35 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Detail from './component/Detail.js';
+import Lesserafim_member from './component/Lesserafim_member.js';
+import NewJeans_member from './component/NewJeans_member.js';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import styled from 'styled-components'
+import Alert from 'react-bootstrap/Alert';
+
 
 function App() {
-  let [lessrafim, setLesserafim] = useState([]);
+  let [lesserafim, setLesserafim] = useState([]);
+  let [newjeans, setNewjeans] = useState([]);
+  let navigate = useNavigate();
 
   useEffect(() => {
     axios.get('https://hyunkyu-song.github.io/GoodsShop/LESSERAFIM/LESSERAFIM.json')
       .then((result) => {
-        console.log(result.data);
+        // console.log(result.data);
         let copy = [...result.data];
         setLesserafim(copy);
+      })
+      .catch(() => {
+        console.log('fail');
+      })
+
+    axios.get('https://hyunkyu-song.github.io/GoodsShop/NewJeans/NewJeans.json')
+      .then((result) => {
+        // console.log(result.data);
+        let copy = [...result.data];
+        setNewjeans(copy);
       })
       .catch(() => {
         console.log('fail');
@@ -26,47 +45,62 @@ function App() {
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Goods Shop</Navbar.Brand>
+          <Navbar.Brand onClick={() => { navigate('/') }}>Goods Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#pricing">TWICE</Nav.Link>
-            <Nav.Link href="#features">LE SSERAFIM</Nav.Link>
-            <Nav.Link href="#pricing">NewJeans</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/TWICE') }}>TWICE</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/LESSERAFIM') }}>LE SSERAFIM</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/NewJeans') }}>NewJeans</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      {/* <div className="main-bg"></div> */}
 
-      {/* <button className='btn btn-outline-warning m-5' onClick={() => {
-        console.log(brr[1].name);
-      }}>버튼</button> */}
+      <Routes>
+        <Route path='/' element={<><h2>home</h2></>
+        } />
 
-      <div className="container mt-4">
-        <div className="row">
-          {
-            lessrafim.map(function (item, i) {
-              return (
-                <Member lessrafim={lessrafim} i={i}/>
-              )
-            })
-          }
-        </div>
-      </div>
+        <Route path='/TWICE' element={
+          <div className="container mt-4">
+            <div className="row">
+              {/* {
+                lesserafim.map(function (item, i) {
+                  return (<Lesserafim_member lesserafim={lesserafim} i={i} />)
+                })
+              } */}
+            </div>
+          </div>
+        } />
+
+        <Route path='/LESSERAFIM' element={
+          <div className="container mt-4">
+            <div className="row">
+              {
+                lesserafim.map(function (item, i) {
+                  return (<Lesserafim_member lesserafim={lesserafim} i={i} />)
+                })
+              }
+            </div>
+          </div>
+        } />
+
+        <Route path='/NewJeans' element={
+          <div className="container mt-4">
+            <div className="row">
+              {
+                newjeans.map(function (item, i) {
+                  return (<NewJeans_member newjeans={newjeans} i={i} />)
+                })
+              }
+            </div>
+          </div>
+        } />
+
+        <Route path='/:group/detail/:groupid/:id' element={<Detail groupName={[lesserafim, newjeans]} />} />
+
+        <Route path="*" element={<div className='container'><h2 className='mt-5'>404 Error</h2></div>} />
+
+      </Routes>
     </>
   );
-}
-
-function Member(props) {
-  return (
-    <>
-      <div className="col-md-4 text-center" key={props.i}>
-        <img src={`https://hyunkyu-song.github.io/GoodsShop/LESSERAFIM/${props.lessrafim[props.i].name}.jpg`} width="80%" />
-        <h4>{props.lessrafim[props.i].name}</h4>
-        <p>{props.lessrafim[props.i].birth}</p>
-        <p>{props.lessrafim[props.i].from}</p>
-        <p>{props.lessrafim[props.i].MBTI}</p>
-      </div>
-    </>
-  )
 }
 
 export default App;
