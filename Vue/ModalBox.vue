489c19cadd8b7eae599324785ad6e18db7ba476e
Song <108565747+HyunKyu-Song@users.noreply.github.com>
2023-04-09 21:06:16 +0900

    Vue

--- Vue/ModalBox.vue
+++ Vue/ModalBox.vue
@@ -1,21 +1,30 @@
 <template>
-  <div class="black-bg" @click="$emit('closeModal')">
+  <div class="black-bg">
     <div class="white-bg">
       <h2>{{ oneroom[num].title }}</h2>
       <h4>{{ oneroom[num].content }}</h4>
+      <!-- <input class="goods" type="text" @input="month = $event.target.value"/> 개월 -->
+      <input class="goods" type="text" v-model="month"/> 개월
       <p>월세: {{ oneroom[num].price }}원</p>
+      <p>값: {{ oneroom[num].price * month }} 원</p>
       <img
         :src="oneroom[num].image"
         alt="room"
         style="cursor: pointer; margin-top: 40px"
       />
     </div>
+    <button @click="$emit('closeModal')" style="cursor: pointer;">닫기</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "ModalBox",
+  data(){
+    return{
+      month : 0,
+    }
+  },
   props: {
     oneroom: Object,
     num: Number
