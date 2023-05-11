b33258dca020f0bef135dd2f168829ac6cc39ba4
Song <108565747+HyunKyu-Song@users.noreply.github.com>
2023-05-11 23:05:50 +0900

    Next.js

--- Next.js/cart/page.js
+++ Next.js/cart/page.js
@@ -1,17 +1,38 @@
-export default function Cart() {
+export default function Cart(props) {
+   let product = [
+      {
+         model: '모니터',
+         price: '187,100'
+      },
+      {
+         model: '키보드',
+         price: '31,790'
+      },
+      {
+         model: '마우스',
+         price: '24,900'
+      },
+   ];
    return (
       <div>
          <h2 className="title">Cart</h2>
-         <div className="cart-item">
-            <p>상품명</p>
-            <p>$40</p>
-            <p>1개</p>
-         </div>
-         <div className="cart-item">
-            <p>상품명</p>
-            <p>$40</p>
-            <p>1개</p>
-         </div>
+         {
+            product.map(function(item, i){
+               return(
+                  <CartList product={product} item={item} i={i}/>
+               )
+            })
+         }
+      </div>
+   )
+}
+
+function CartList(props) {
+   return (
+      <div className="cart-item">
+         <p>{props.item.model}</p>
+         <p>{props.item.price}</p>
+         <p>1개</p>
       </div>
    )
 }
