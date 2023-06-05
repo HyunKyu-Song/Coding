760109dd87a1305642ad1904dc07ee5737413ffa
Song <108565747+HyunKyu-Song@users.noreply.github.com>
2023-06-05 23:48:54 +0900

    TypeScript

--- TypeScript/연습.ts
+++ TypeScript/연습.ts
@@ -7,20 +7,20 @@ let 뉴진스: { 멤버: string[], age: number[] } = {
    age: [20, 20, 19, 18, 16]
 }
 
-let project :{member:string[], days:number, started:boolean}= {
+let project: { member: string[], days: number, started: boolean } = {
    member: ['kim', 'park'],
    days: 30,
    started: true,
 }
 
 
-let 학교 :{score:(number|boolean)[], teacher:string, friend:string[] | string} = {
-   score : [100, 97, 84],
-   teacher : 'Phil',
-   friend : 'John'
+let 학교: { score: (number | boolean)[], teacher: string, friend: string[] | string } = {
+   score: [100, 97, 84],
+   teacher: 'Phil',
+   friend: 'John'
 }
 학교.score[4] = false;
-학교.friend = ['Lee' , 학교.teacher]
+학교.friend = ['Lee', 학교.teacher]
 
 // function func1(x? :string) {
 //    if(typeof x == 'undefined'){
@@ -38,3 +38,63 @@ let 학교 :{score:(number|boolean)[], teacher:string, friend:string[] | string}
 // func2('123234')
 
 
+let aa = document.querySelector('.title');
+
+if (aa instanceof Element) {
+   aa.innerHTML = 'haha'
+}
+
+// document.querySelector('.title')?.innerHTML = 'ahahaha';
+
+// document.querySelector('.link').style.color = 'red';
+
+let bb = document.querySelector('.link');
+
+if (bb instanceof HTMLAnchorElement) {
+   bb.style.color = 'coral'
+}
+
+// let cc = document.querySelector('.btn');
+
+// if (cc instanceof HTMLButtonElement) {
+//    cc.addEventListener('click', function () {
+//       if (bb instanceof HTMLAnchorElement) {
+//          bb.style.color = 'blue'
+//       }
+//    })
+// }
+
+// document.querySelector('.btn')?.addEventListener('click', function (){
+//    let dd = document.querySelector('.img');
+//    if(dd instanceof HTMLImageElement){
+//       dd.src = './hanni.jpg';
+//    }
+// })
+
+interface A {
+   name :string;
+   age :number;
+}
+
+interface A{
+   first? :string
+}
+
+let obj1 :A = {
+   name : 'song',
+   age : 20,
+   first : 'hello'
+}
+
+interface B extends A{
+   birth :number
+}
+
+let obj2 :B = {
+   name :'kim',
+   age: 21,
+   birth :98
+}
+
+console.log(obj1)
+console.log(obj2)
