7079f4d0b5dbe667678460ef2f93958335b7aa76
송현규 <108565747+HyunKyu-Song@users.noreply.github.com>
2023-07-23 23:18:27 +0900

    MySQL 연결해서 작성한 글 저장

--- NodeJS 2회차/server.js
+++ NodeJS 2회차/server.js
@@ -1,22 +1,55 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+require('dotenv').config();
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(8080, ()=>{
+var mysql = require('mysql2');
+var connection = mysql.createConnection({
+   host: process.env.DATABASE_HOST,
+   user: process.env.DATABASE_USER,
+   password: process.env.DATABASE_PW,
+   database: process.env.DATABASE
+});
+
+
+app.listen(8080, () => {
    console.log('Listening on 8008~');
 });
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
    res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/write', (req, res)=>{
+app.get('/write', (req, res) => {
    res.sendFile(__dirname + '/write.html');
 });
 
-app.post('/add', (req, res)=>{
-   res.send('success!')
+app.get('/list', (req, res)=>{
+   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/add', (req, res) => {
+   var len;
+
+   connection.connect();
+   connection.query(`SELECT COUNT(*) AS len FROM post;`, function (error, results, fields) {
+      if (error) throw (error);
+
+      len = results[0].len;
+
+      connection.query(`
+         INSERT INTO mydatabase.post 
+            (post_id, post_work, date)
+         VALUES
+            ('${len}', '${req.body.work}', '${req.body.date}');`
+         , function (error, results, fields) {
+            if (error) throw (error);
+         });
+   });
+   connection.end();
+
+   res.redirect('/');
    console.log(req.body)
 });
