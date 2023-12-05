10a53e23c0227f2d33d1e6665f2a3a53e6e66b6f
송현규 <108565747+HyunKyu-Song@users.noreply.github.com>
2023-12-05 23:34:01 +0900

    백준 문제

---
+++ C++/사칙연산.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	int x, y;
+	
+	cin >> x >> y;
+	cout << x+y << "\n";
+	cout << x-y << "\n";
+	cout << x*y << "\n";
+	cout << x/y << "\n";
+	cout << x%y;
+	
+	return 0;
+}
