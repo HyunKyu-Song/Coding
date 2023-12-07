88146c40c57a72418f9029cb5c50fb6103736457
송현규 <108565747+HyunKyu-Song@users.noreply.github.com>
2023-12-07 20:05:52 +0900

    Add files via upload

---
+++ C++/백준/A+B - 3.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	int n, x, y;
+	
+	scanf("%d", &n);
+	
+	for(int i = 0; i < n; i++){
+		
+	scanf("%d %d", &x, &y);
+		printf("%d\n", x+y);
+	}
+	
+	return 0;
+}
